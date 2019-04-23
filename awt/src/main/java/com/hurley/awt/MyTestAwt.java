c53ce50248d4e858e0f852a2b0c3aece8008b11c
hurleyzz <694384748@qq.com>
2019-04-23 14:46:49 +0800

    Initial commit

---
+++ awt/src/main/java/com/hurley/awt/MyTestAwt.java
@@ -0,0 +1,12 @@
+package com.hurley.awt;
+
+/**
+ * Created by hurley, Date on 19/4/23.
+ */
+public class MyTestAwt {
+    private static String name = "test name";
+
+    public static String getName() {
+        return name;
+    }
+}
